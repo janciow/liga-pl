6d3d05e907856085b59e39240f793ef4f433fdb2
jan.bancarewicz <jan.bancarewicz@gmail.com>
2018-04-30 09:03:41 +0200

    added billing and client site

---
+++ client/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => {
+  return <div><h2>Ankieta</h2><p>podoba sie logoanie?</p></div>;
+};
+const SurveyNew = () => <h2>nowa ankieta</h2>;
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Polska Liga
+      </h1>
+      <p>Tabela, wyniki, rozkład meczykow</p>
+    </div>
+  )
+}
+
+export default Landing;
