5b60eb1530077ec37479eccd5036f369aca903a8
jan.bancarewicz <jan.bancarewicz@gmail.com>
2018-04-30 09:05:57 +0200

    add client and billing

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys')
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 20 * 24 * 60 * 60 * 1000,
@@ -20,6 +22,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.hasUncaughtExceptionCaptureCallback.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js fire, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
